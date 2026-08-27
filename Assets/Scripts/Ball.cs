fd2e17f56b71ede775bc8b1c7163259c5b222f6b
Shifuya-sol <sitangtangsrisuwan@gmail.com>
2026-08-27 22:09:11 +0700

    Finish SetBall

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,7 +4,7 @@ public enum BallColor
 {
         White,
         Red,
-        Yelllow,
+        Yellow,
         Green,
         Brown,
         Blue,
@@ -22,6 +22,9 @@ public class Ball : MonoBehaviour,IPointerClickHandler
     [SerializeField]
     private BallColor color;
 
+    [SerializeField]
+    private MeshRenderer rd; 
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
@@ -29,6 +32,10 @@ public class Ball : MonoBehaviour,IPointerClickHandler
         Destroy(gameObject);
     }
 
+    private void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -40,5 +47,45 @@ public class Ball : MonoBehaviour,IPointerClickHandler
     void Update()
     {
         
+    }
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+
+        }
     }
 }
