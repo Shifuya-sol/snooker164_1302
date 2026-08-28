9783b479499fd389bfac819b7d098048ed044778
Shifuya-sol <sitangtangsrisuwan@gmail.com>
2026-08-28 18:36:30 +0700

    Audio

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -18,6 +18,7 @@ public class Hole : MonoBehaviour
                 GameManager.instance.ShowScoreText(b.Point);
             }
             Destroy(b.gameObject);
+            AudioManager.instance.PlaySFX(2);
         }
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AudioManager.instance.PlayBGM(0);
     }
 
     // Update is called once per frame
